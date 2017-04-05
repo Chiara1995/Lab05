5c9ada9cf7cd0d92bd5250c6616baa6c8d42b66a
chiar <chiar@LAPTOP-M50N8B04>
2017-04-05 13:49:13 +0200

    Calcolo anagrammi della parola con algoritmo ricorsivo

--- Lab5_Anagrammi/src/it/polito/tdp/anagrammi/model/Model.java
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/model/Model.java
@@ -37,6 +37,12 @@ public class Model {
 		}
 	}
 	
+	/**
+	 * Restituisce il numero di occorrenze del carattere nell'array
+	 * @param caratteri
+	 * @param c
+	 * @return
+	 */
 	private int numeroLetteraLista(Character [] caratteri, Character c){
 		int numLettere=0;
 		for(int i=0; i<caratteri.length; i++){
@@ -47,6 +53,12 @@ public class Model {
 		return numLettere;
 	}
 	
+	/**
+	 * Restituisce il numero di occorrenze del carattere nella stringa
+	 * @param stringa
+	 * @param c
+	 * @return
+	 */
 	private int numeroLetteraStringa(String stringa, Character c){
 		int numLettere=0;
 		for(int i=0; i<stringa.length(); i++){
@@ -58,15 +70,19 @@ public class Model {
 	}
 	
 	private void scegli(String parziale, int livello){
+		//Condizione di terminazione
 		if(parziale.length()==caratteri.length){
 			if(!anagrammi.contains(parziale))
 				anagrammi.add(parziale);
 			return;
 		}
+		//Generazione di una nuova soluzione
 		for(Character c : caratteri){
+			//Filtro sulla chiamata ricorsiva
 			if(numeroLetteraStringa(parziale, c)<numeroLetteraLista(caratteri,c)){
 				parziale+=c;
 				scegli(parziale, livello+1);
+				//Backtracking
 				parziale=parziale.substring(0, parziale.length()-1);
 			}
 		}
