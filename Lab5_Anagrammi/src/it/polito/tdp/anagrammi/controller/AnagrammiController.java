19725f467d060d14002f25b401339236e36aee98
chiar <chiar@LAPTOP-M50N8B04>
2017-04-05 11:52:46 +0200

    Interfaccia grafica

--- Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
@@ -1,8 +1,57 @@
+/**
+ * Sample Skeleton for 'Anagrammi.fxml' Controller Class
+ */
+
 package it.polito.tdp.anagrammi.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class AnagrammiController {
 
-	
-}
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
 
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="txtParola"
+    private TextField txtParola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCalcolaAnagrammi"
+    private Button btnCalcolaAnagrammi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtAnagrammiCorretti"
+    private TextArea txtAnagrammiCorretti; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtAnagrammiErrati"
+    private TextArea txtAnagrammiErrati; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnReset"
+    private Button btnReset; // Value injected by FXMLLoader
+
+    @FXML
+    void doCalcolaAnagrammi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert btnCalcolaAnagrammi != null : "fx:id=\"btnCalcolaAnagrammi\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert txtAnagrammiCorretti != null : "fx:id=\"txtAnagrammiCorretti\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert txtAnagrammiErrati != null : "fx:id=\"txtAnagrammiErrati\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+
+    }
+}
